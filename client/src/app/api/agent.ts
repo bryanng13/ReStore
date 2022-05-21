8f228a0ca154bd65a56a58f827c81b64d375331b
Bryan Ng <bryanng05@gmail.com>
2022-05-22 02:17:37 +1200

    End of section 6

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { history } from '../..';
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
@@ -53,6 +54,12 @@ const Catalog = {
     details: (id: number) => requests.get(`products/${id}`)
 }
 
+const Basket = {
+    get: () => requests.get('basket'),
+    addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+    removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`)
+}
+
 const TestErrors = {
     get400Error: () => requests.get('buggy/bad-request'),
     get401Error: () => requests.get('buggy/unauthorised'),
@@ -62,6 +69,7 @@ const TestErrors = {
 }
 
 const agent = {
+    Basket,
     Catalog,
     TestErrors
 }
